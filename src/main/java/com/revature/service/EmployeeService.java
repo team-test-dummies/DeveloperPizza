1e7f756b06e6fb89095a3593c5e2c4c81d4c810b
Joshua Gibbs <33700586+joshua649Revature@users.noreply.github.com>
2022-11-09 10:10:32 -0500

    Add files via upload

---
+++ src/main/java/com/revature/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.revature.services;
+
+import com.revature.daos.EmployeeDao;
+import com.revature.models.Employee;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class EmployeeService {
+    private EmployeeDao employeeDao = new EmployeeDao();
+
+    public List<Employee> getAllEmployees() throws SQLException {
+        return employeeDao.getAllEmployees();
+    }
+}
