1cf8c91270f1af6f5495b3947157a563f5d9c780
David Hanzlik <david420@revature.net>
2022-11-09 13:05:53 -0600

    fixed package clause for service classes

--- src/main/java/com/revature/service/DeveloperService.java
+++ src/main/java/com/revature/service/DeveloperService.java
@@ -1,4 +1,4 @@
-package com.revature.services;
+package com.revature.service;
 
 import com.revature.daos.DeveloperDao;
 import com.revature.models.Developer;
--- src/main/java/com/revature/service/EmployeeService.java
+++ src/main/java/com/revature/service/EmployeeService.java
@@ -1,4 +1,4 @@
-package com.revature.services;
+package com.revature.service;
 
 import com.revature.daos.EmployeeDao;
 import com.revature.models.Employee;
