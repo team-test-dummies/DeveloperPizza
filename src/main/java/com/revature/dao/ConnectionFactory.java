9b942f6d8e40785224abef6d1d7e75c53dc1445d
David Hanzlik <david420@revature.net>
2022-11-11 16:57:42 -0600

    removing environment file dependency, set environment variables using a run configuration or globals

--- src/main/java/com/revature/dao/ConnectionFactory.java
+++ src/main/java/com/revature/dao/ConnectionFactory.java
@@ -9,20 +9,16 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
     public static Connection createConnection() throws SQLException {
-        // Dotenv object created
-        Dotenv dotenv = Dotenv.configure().directory("./src/").load();
-        // Setup Connection With DB
 
-        // Setup the environment file 'Jeremy' was talking about
-        String url = dotenv.get("DB_URL");
-        String username = dotenv.get("DB_USER");
-        String password = dotenv.get("DB_PASS");
+        String url = System.getenv("DB_URL");
+        String username = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASS");
 
         Driver postgresDriver = new Driver();
         DriverManager.registerDriver(postgresDriver);
 
         Connection connection = DriverManager.getConnection(url, username, password);
-        System.out.println(connection);
+
         return connection;
     }
 }
