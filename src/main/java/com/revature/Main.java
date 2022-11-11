0593ced86131681780f634266a01a3e8226f1508
Joshua Gibbs <jgibbs9076@yahoo.com>
2022-11-11 09:39:16 -0500

    Updated

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -1,5 +1,6 @@
 package com.revature;
 
+import com.revature.controller.OrderController;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
@@ -14,6 +15,10 @@ public class Main {
                 staticConfig.location = Location.CLASSPATH;
             });
         });
+
+        // TEST ORDER ENDPOINTS
+        OrderController orderController = new OrderController();
+        orderController.mapEndpoint(app);
 
         // start the server
         app.start(8080);
