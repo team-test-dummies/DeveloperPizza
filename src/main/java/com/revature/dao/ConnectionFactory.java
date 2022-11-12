9f9c91b8ee80976f37e4a0aa92da426c9f0fcdbd
David Hanzlik <david420@revature.net>
2022-11-12 05:30:52 -0600

    removed unnecessary driver registration

--- src/main/java/com/revature/dao/ConnectionFactory.java
+++ src/main/java/com/revature/dao/ConnectionFactory.java
@@ -1,27 +1,17 @@
 package com.revature.dao;
 
-import org.postgresql.Driver;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
-    public static Connection createConnection() throws SQLException {
-        // Dotenv object created
+public interface ConnectionFactory {
 
-        // Setup Connection With DB
+    static String url = System.getenv("DB_URL");
+    static String username = System.getenv("DB_USER");
+    static String password = System.getenv("DB_PASS");
 
-        // Setup the environment file 'Jeremy' was talking about DB_URL, DB_USER, DB_PASS
-        String url = System.getenv("DB_URL");
-        String username = System.getenv("DB_USER");
-        String password = System.getenv("DB_PASS");
-
-        Driver postgresDriver = new Driver();
-        DriverManager.registerDriver(postgresDriver);
-
+    static Connection createConnection() throws SQLException {
         Connection connection = DriverManager.getConnection(url, username, password);
-        System.out.println(connection);
         return connection;
     }
 }
