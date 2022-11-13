f8aa8ceb611947431b58130be6c9b53892e24efe
Jeremy F McKay <jeremyfmckay@gmail.com>
2022-11-13 13:52:37 -0800

    login on frontend working

--- src/main/resources/public/scripts/index.js
+++ src/main/resources/public/scripts/index.js
@@ -1,32 +1,22 @@
-const baseUrl = 'http:localhost:8080/login';
+const baseUrl = 'http://127.0.0.1:8080';
 
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const loginButton = document.getElementById('login');
 
 loginButton.addEventListener('click', () => {
-    const username = usernameInput.value;
-    const password = passwordInput.value;
-    
     fetch(`${baseUrl}/login`, {
         method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-        username,
-        password,
-        }),
-    })
-        .then((response) => {
-            console.log(response.json());
-            response.json()
-        })
-        .then((data) => {
-        if (data.message) {
-            alert(data.message);
+        body: `{"username":"${usernameInput.value}","password":"${passwordInput.value}"}`,
+        credentials: 'include' // Very important so that the browser will retain the Cookie when we log in
+        // by default the browser will throw the Cookie away
+    }).then((res) => {
+        alert(res.status);
+        if (res.status === 200) {
+            alert("found login")
+            window.location.href = '/pages/startorder.html';
         } else {
-            console.log("Login successful");
+            alert('Invalid login!');
         }
-        });
+    });
 });
