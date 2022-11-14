e5c580e0d667ab154b2f2bbbb2db7689b6544ce8
Jeremy F McKay <jeremyfmckay@gmail.com>
2022-11-14 11:06:21 -0800

    Minor commit after a few changes

--- src/main/resources/public/scripts/index.js
+++ src/main/resources/public/scripts/index.js
@@ -1,21 +1,23 @@
 const baseUrl = 'http://127.0.0.1:8080';
 
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
-const loginButton = document.getElementById('login');
+document.addEventListener('DOMContentLoaded', () => {
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const loginButton = document.getElementById('login');
+    const error = document.getElementById('error');
 
-loginButton.addEventListener('click', () => {
-    fetch(`${baseUrl}/login`, {
-        method: 'POST',
-        body: `{"username":"${usernameInput.value}","password":"${passwordInput.value}"}`,
-        credentials: 'include' // Very important so that the browser will retain the Cookie when we log in
-        // by default the browser will throw the Cookie away
-    }).then((res) => {
-        alert(res.status);
-        if (res.status === 204) {
-            window.location.href = '/pages/startorder.html';
-        } else {
-            alert('Invalid login!');
-        }
+    loginButton.addEventListener('click', () => {
+        fetch(`${baseUrl}/login`, {
+            method: 'POST',
+            body: `{"username":"${usernameInput.value}","password":"${passwordInput.value}"}`,
+            credentials: 'include' // Very important so that the browser will retain the Cookie when we log in
+            // by default the browser will throw the Cookie away
+        }).then((res) => {
+            if (res.status === 204) {
+                window.location.href = '/pages/startorder.html';
+            } else {
+                error.innerHTML = 'Invalid username or password';
+            }
+        });
     });
 });
