f633e79b71272695e39cac90350b41db6e912667
David Hanzlik <david420@revature.net>
2022-11-16 13:02:41 -0600

    created positive login test

--- src/test/java/com/revature/controller/AuthControllerTest.java
+++ src/test/java/com/revature/controller/AuthControllerTest.java
@@ -1,4 +1,56 @@
 package com.revature.controller;
 
+import com.revature.App;
+import com.revature.PrototypingApp;
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import okhttp3.Response;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+import java.util.Map;
+
 public class AuthControllerTest {
+
+    private static Javalin app;
+    @BeforeTest
+    public void setup() throws SQLException {
+        PrototypingApp.setup();
+        app = App.initialize();
+    }
+
+    @AfterTest
+    public void cleanup() throws SQLException {
+        PrototypingApp.cleanup();
+        app = null;
+    }
+
+    @Test
+    public static void loginPositive() {
+        // Arrange
+        String username = "employer";
+        String password = "guest";
+
+        // Act
+        JavalinTest.test(app, (server, client) -> {
+            Response response = client.post(
+                    "/login",
+                    Map.of(
+                            "username", username,
+                            "password", password
+                    )
+            );
+
+            // Assert
+            Assert.assertTrue(
+                response.header("Set-Cookie").contains("JSESSIONID"),
+                "Expected Set-Cookie header with JSESSIONID but found none"
+            );
+        });
+
+    }
+
 }
