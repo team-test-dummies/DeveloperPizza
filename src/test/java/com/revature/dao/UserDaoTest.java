11fa258d7693fc3d1527240298304a33d4f94321
smcqueen186 <mcqueen.ston@gmail.com>
2022-11-18 05:56:31 -0500

    unit tests for userdao

--- src/test/java/com/revature/dao/UserDaoTest.java
+++ src/test/java/com/revature/dao/UserDaoTest.java
@@ -3,12 +3,15 @@ package com.revature.dao;
 import com.revature.PrototypingApp;
 import com.revature.data.enums.exception.UserUnsuccessfullyAddedException;
 import com.revature.data.records.Customer;
+import com.revature.data.records.DeleteAccountInfo;
+import com.revature.data.records.EditProfile;
 import com.revature.data.records.RegisterInfo;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserDaoTest {
     @BeforeMethod
@@ -20,17 +23,8 @@ public class UserDaoTest {
     public void cleanup() throws SQLException {
         PrototypingApp.cleanup();
     }
-    @Test
-    public void getAllCustomersPositiveHackTest() throws SQLException, IOException {
-        //Arrange
-
-        int expected = 4;
-
-        //Act
-        int actual = UserDao.getAllCustomers().size();
-
-        //Assert
-        //Assert.assertEquals(actual, expected);
+    @Test(enabled = false)
+    public void getAllCustomersPositive() throws SQLException, IOException {
 
     }
 
@@ -43,11 +37,11 @@ public class UserDaoTest {
                 "password",
                 "555-555-5555",
                 "jane@gmail.com",
-                "Atlanta, GA");
+                "Atlanta, GA"
+        );
 
         int expected = 1;
         int actual = UserDao.registerCustomer(newUser);
-
         Assert.assertEquals(actual, expected);
     }
 
@@ -61,11 +55,11 @@ public class UserDaoTest {
                 "password",
                 "555-555-5555",
                 "",
-                "Atlanta, GA");
+                "Atlanta, GA"
+        );
 
         int expected = 0;
         int actual = UserDao.registerCustomer(invalidInfo);
-
         Assert.assertEquals(actual, expected);
     }
 
@@ -81,14 +75,74 @@ public class UserDaoTest {
                 "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��",
                 "505-684-9399",
                 "madkor436@company.net",
-                "New Mexico");
+                "New Mexico"
+        );
         Customer actual = UserDao.getCustomerByUsername(username);
-
         Assert.assertEquals(actual, expected);
     }
 
     @Test
-    public void getUserNegative() {
+    public void getUserNegative() throws SQLException {
+        String username = "invalid";
 
+        Customer expected = null;
+        Customer actual = UserDao.getCustomerByUsername(username);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void editUserPositive() throws SQLException {
+        EditProfile editedInfo = new EditProfile(
+                "madison_kora",
+                "invalidUsername",
+                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��",
+                "505-684-9399",
+                "madkor436@company.net",
+                "New Mexico"
+        );
+
+        int expected = 0;
+        int actual = UserDao.editCustomer(editedInfo);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void editUserNegative() throws SQLException {
+        EditProfile editedInfo = new EditProfile(
+                "new_name",
+                "new",
+                "newPassword",
+                "555-555-5555",
+                "new@gmail.com",
+                "New Mexico"
+        );
+
+        int expected = 0;
+        int actual = UserDao.editCustomer(editedInfo);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void removeUserPositive() throws SQLException, IOException {
+        DeleteAccountInfo validCredentials = new DeleteAccountInfo(
+                "madkor436@company.net",
+                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��"
+        );
+
+        int expected = 1;
+        int actual = UserDao.removeCustomerUsingCredentials(validCredentials);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void removeUserNegative() throws SQLException, IOException {
+        DeleteAccountInfo invalidCredentials = new DeleteAccountInfo(
+                "madkor436@company.net",
+                "invalid"
+        );
+
+        int expected = 0;
+        int actual = UserDao.removeCustomerUsingCredentials(invalidCredentials);
+        Assert.assertEquals(actual, expected);
     }
 }
--- src/test/java/com/revature/service/UserServiceTest.java
+++ src/test/java/com/revature/service/UserServiceTest.java
@@ -30,135 +30,149 @@ public class UserServiceTest {
     }
 
     // VIEW ALL CUSTOMERS
-    @Test
+    @Test(enabled = false)
     public void getAllCustomersTest() throws SQLException, IOException {
-        //Arrange
-
-        //Act
-
-        //Assert
 
     }
 
     // -- REGISTER CUSTOMERS --
     @Test
     public void registerCustomerTestPositive() throws SQLException {
-        //Arrange
 
-        RegisterInfo newCustomerInfo = new RegisterInfo("CUSTOMER", "John Doe", "jonnie",
-                "password", "555-555-5555", "john@gmail.com", "Georgia");
+        RegisterInfo newCustomerInfo = new RegisterInfo(
+                "CUSTOMER",
+                "John Doe",
+                "jonnie",
+                "password",
+                "555-555-5555",
+                "john@gmail.com",
+                "Georgia"
+        );
 
-        //Act
         int expected = 1;
         int actual = UserService.registerCustomer(newCustomerInfo);
-
-        //Assert
         Assert.assertEquals(actual, expected);
     }
 
     @Test(expectedExceptions = com.revature.data.enums.exception.UserUnsuccessfullyAddedException.class)
     public void registerCustomerTestNegative() throws SQLException {
-        //Arrange
-        RegisterInfo noInfo = new RegisterInfo("CUSTOMER", "John Doe", "",
-                "password", "555-555-5555", "", "Georgia");
 
+        RegisterInfo noInfo = new RegisterInfo(
+                "CUSTOMER",
+                "John Doe",
+                "",
+                "password",
+                "555-555-5555",
+                "",
+                "Georgia"
+        );
         Exception exception = new AccountUnsuccessfullyEditedException("Account was not created");
 
-        //Act
         Exception expected = exception;
         int actual = UserService.registerCustomer(noInfo);
-
-        //Assert
         Assert.assertEquals(actual, expected);
     }
 
     // -- VIEW CUSTOMER BY USERNAME --
     @Test
     public void getCustomerByUsernamePositive() throws SQLException {
-        //Arrange
-        Customer madisonKora = new Customer(1, "CUSTOMER", "madison_kora", "madkor436",
-                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��", "505-684-9399", "madkor436@company.net", "New Mexico");
 
-        //Act
+        Customer madisonKora = new Customer(
+                1,
+                "CUSTOMER",
+                "madison_kora",
+                "madkor436",
+                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��",
+                "505-684-9399",
+                "madkor436@company.net",
+                "New Mexico"
+        );
+
         Customer expected = madisonKora;
         Customer actual = UserService.getCustomerByUsername("madkor436");
-
-        //Assert
         Assert.assertEquals(actual, expected);
     }
 
     @Test(expectedExceptions = com.revature.data.enums.exception.UserNotFoundException.class)
     public void getCustomerByUsernameNegative() throws SQLException {
-        //Arrange
-        Customer customer = new Customer(1, "CUSTOMER", "madison_kora", "madkor436",
-                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��", "505-684-9399", "madkor436@company.net", "New Mexico");
 
+        Customer customer = new Customer(1,
+                "CUSTOMER",
+                "madison_kora",
+                "madkor436",
+                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��",
+                "505-684-9399",
+                "madkor436@company.net",
+                "New Mexico"
+        );
         Exception exception = new UserNotFoundException("User does not exist");
 
-        //Act
         Exception expected = exception;
         Customer actual = UserService.getCustomerByUsername("invalidUsername");
-
-        //Assert
         Assert.assertSame(actual, expected);
     }
 
     // -- EDIT CUSTOMER PROFILE --
     @Test
     public void editCustomerTestPositive() throws SQLException, IOException {
-        //Arrange
-        EditProfile editedProfile = new EditProfile("madison_kora", "madkor436",
-                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��", "555-555-5555", "madkor436@company.net", "California");
 
-        //Act
+        EditProfile editedProfile = new EditProfile(
+                "madison_kora",
+                "madkor436",
+                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��",
+                "555-555-5555",
+                "madkor436@company.net",
+                "California"
+        );
+
         EditProfile expected = editedProfile;
         EditProfile actual = UserService.editCustomer(editedProfile);
-
-        //Assert
         Assert.assertEquals(actual, expected);
     }
 
     @Test(expectedExceptions = com.revature.data.enums.exception.AccountUnsuccessfullyEditedException.class)
     public void editCustomerTestNegative() throws SQLException, IOException {
-        //Arrange
-        EditProfile editedProfile = new EditProfile("madison_kora", "INVALID",
-                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��", "555-555-5555", "madkor436@company.net", "California");
 
+        EditProfile editedProfile = new EditProfile(
+                "madison_kora",
+                "INVALID",
+                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��",
+                "555-555-5555",
+                "madkor436@company.net",
+                "California"
+        );
         Exception exception = new AccountUnsuccessfullyEditedException("Profile was not edited");
 
-        //Act
         Exception expected = exception;
         EditProfile actual = UserService.editCustomer(editedProfile);
-
-        //Assert
         Assert.assertEquals(actual, expected);
     }
 
     // -- DELETE CUSTOMER PROFILE --
     @Test
     public void removeCustomerTestPositive() throws SQLException, IOException { // NEED DELETE ORDERS METHOD IN ORDER TO DELETE USER
-        //Arrange
-        DeleteAccountInfo credentials = new DeleteAccountInfo("madkor436@company.net",
-                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��");
-        //Act
+
+        DeleteAccountInfo credentials = new DeleteAccountInfo(
+                "madkor436@company.net",
+                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��"
+        );
+
         int expected = 1;
         int actual = UserService.removeCustomerUsingCredentials(credentials);
-
-        //Assert
         Assert.assertEquals(actual, expected);
     }
 
     @Test(expectedExceptions = com.revature.data.enums.exception.AccountUnsuccessfullyRemovedException.class)
     public void removeCustomerTestNegative() throws SQLException, IOException {
-        //Arrange
-        DeleteAccountInfo credentials = new DeleteAccountInfo("invalid",
-                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��");
+
+        DeleteAccountInfo credentials = new DeleteAccountInfo(
+                "invalid",
+                "k�5�O���\u0015D�a=�z��kl\\q�I���\u000F�x��"
+        );
         Exception exception = new AccountUnsuccessfullyRemovedException("Profile was not removed");
-        //Act
+
         Exception expected = exception;
         int actual = UserService.removeCustomerUsingCredentials(credentials);
-
-        //Assert
         Assert.assertEquals(actual, expected);
     }
 }
