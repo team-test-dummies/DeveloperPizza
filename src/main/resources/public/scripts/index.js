dd3615baa37cdd614b51576b46c728367c008867
Jeremy F McKay <jeremyfmckay@gmail.com>
2022-11-20 22:05:20 -0800

    Even more cosmetic Selenium breaking changes.

--- src/main/resources/public/scripts/index.js
+++ src/main/resources/public/scripts/index.js
@@ -1,6 +1,5 @@
-
-document.addEventListener('DOMContentLoaded', () => {   
-
+// I like to make sure that the content is loaded.
+document.addEventListener('DOMContentLoaded', () => {
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
     const loginButton = document.getElementById('login');
@@ -15,11 +14,24 @@ document.addEventListener('DOMContentLoaded', () => {
         }).then((res) => {
             if (res.status === 204) {
                 window.location.href = '/pages/startorder.html';
-
-            } else {
-                alert('Invalid username or password');
+            } else if (res.status === 401) {
+                tempMessage("Invalid username or password");
+            } else if (res.status === 403) {
+                tempMessage("Account is not active");
+            } else if (res.status === 400) {
+                tempMessage("Fields cannot be empty");
             }
+        }).catch (err => {
+            console.log(err);
         });
     });
+
+    function tempMessage(message) {
+        error.innerHTML = message;
+        error.style.opacity = '1';
+        setTimeout(function() {
+            error.style.opacity =   "0";
+        }, 3000);
+    }
 
 });
