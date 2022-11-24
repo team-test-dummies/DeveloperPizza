bf1c3d267c48cfc8053dca884c1a5fb7fc364dea
Joshua Gibbs <jgibbs9076@yahoo.com>
2022-11-24 12:04:59 -0500

    Added More Elements

--- src/test/java/com/revature/pages/ProfilePage.java
+++ src/test/java/com/revature/pages/ProfilePage.java
@@ -23,14 +23,13 @@ public class ProfilePage {
 
     // GET ACCOUNT DETAILS
     public boolean account_details() {
-        boolean accountDetails = MainRunner.driver.findElement(By.xpath("//ul[@id='accountInfo']")).isDisplayed();
+        boolean accountDetails = MainRunner.driver.findElement(By.xpath("//div[@id='accountInfo']")).isDisplayed();
         return accountDetails;
     }
 
     // GET ACCOUNT USERNAME
     @FindBy(xpath = "//div[1]//div[1]/ul//li[1]")
     public WebElement accountUsername;
-
 
     // GET ACCOUNT FULLNAME
     @FindBy(xpath = "//div[1]//div[1]/ul//li[2]")
@@ -50,7 +49,62 @@ public class ProfilePage {
 
     // GET ORDER LIST
     public boolean order_list() {
-        boolean orderList = MainRunner.driver.findElement(By.xpath("//ul[@id='orders']")).isDisplayed();
-        return orderList;
+        return MainRunner.driver.findElement(By.xpath("//ul[@id='orders']")).isDisplayed();
     }
+
+    // EDIT BUTTON
+    @FindBy(xpath = "//ul[1]/li[1]/ul[1]/li[7]/button[1]")
+    public WebElement editOrderButton;
+
+    // EDIT ORDER BOX (POPUP)
+    @FindBy(xpath = "//body/order-editor[1]/dialog[1]")
+    public WebElement editOrderBox;
+
+    // EDIT NAME
+    @FindBy(xpath = "//label[2]/input[1]")
+    public WebElement editName;
+
+    // EDIT LANGUAGES
+    @FindBy(xpath = "//label[3]//select[1]")
+    public WebElement editLanguage;
+
+    // EDIT TOOLS
+    @FindBy(xpath = "//label[4]/select[1]")
+    public WebElement editTool;
+
+    // EDIT EDUCATION
+    @FindBy(xpath = "//label[5]/select[1]")
+    public WebElement editEducation;
+
+    // EDIT SALARY
+    @FindBy(xpath = "//label[6]/input[1]")
+    public WebElement editSalary;
+
+    // CONFIRM BUTTON
+    @FindBy(xpath = "//button[contains(text(),'Confirm')]")
+    public WebElement confirmOrderButton;
+
+    // DELETE BUTTON
+    @FindBy(xpath = "//ul[1]/li[1]/ul[1]/li[7]/button[2]")
+    public WebElement deleteOrderButton;
+
+    // GET ORDER NAME
+    @FindBy(xpath = "//ul[1]/li[1]/ul[1]/li[2]/output[1]")
+    public WebElement orderName;
+
+    // GET ORDER LANGUAGE
+    @FindBy(xpath = "//ul[1]/li[1]/ul[1]/li[3]/output[1]")
+    public WebElement orderLanguage;
+
+    // GET ORDER TOOL
+    @FindBy(xpath = "//ul[1]/li[1]/ul[1]/li[4]/output[1]")
+    public WebElement orderTool;
+
+    // GET ORDER EDUCATION
+    @FindBy(xpath = "//ul[1]/li[1]/ul[1]/li[5]/output[1]")
+    public WebElement orderEducation;
+
+    // GET ORDER SALARY
+    @FindBy(xpath = "//ul[1]/li[1]/ul[1]/li[6]/output[1]")
+    public WebElement orderSalary;
 }
